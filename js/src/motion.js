df0ef83a3faf7182fb83149b5ad40f9abca3d323
nhw649 <1433394534@qq.com>
2020-11-26 13:42:38 +0800

    Site updated: 2020-11-26 13:42:35

--- js/src/motion.js
+++ js/src/motion.js
@@ -51,27 +51,52 @@ $(document).ready(function () {
     this.el.velocity('stop').velocity(this.status[status]);
   };
 
+  // var sidebarToggleLine1st = new SidebarToggleLine({
+  //   el: '.sidebar-toggle-line-first',
+  //   status: {
+  //     arrow: {width: '50%', rotateZ: '-45deg', top: '2px'},
+  //     close: {width: '100%', rotateZ: '-45deg', top: '5px'}
+  //   }
+  // });
+  // var sidebarToggleLine2nd = new SidebarToggleLine({
+  //   el: '.sidebar-toggle-line-middle',
+  //   status: {
+  //     arrow: {width: '90%'},
+  //     close: {opacity: 0}
+  //   }
+  // });
+  // var sidebarToggleLine3rd = new SidebarToggleLine({
+  //   el: '.sidebar-toggle-line-last',
+  //   status: {
+  //     arrow: {width: '50%', rotateZ: '45deg', top: '-2px'},
+  //     close: {width: '100%', rotateZ: '45deg', top: '-5px'}
+  //   }
+  // });
   var sidebarToggleLine1st = new SidebarToggleLine({
     el: '.sidebar-toggle-line-first',
     status: {
-      arrow: {width: '50%', rotateZ: '-45deg', top: '2px'},
-      close: {width: '100%', rotateZ: '-45deg', top: '5px'}
+      arrow: {width: '50%', rotateZ: '45deg', top: '2px', left: '6px'},
+      // close: {width: '100%', rotateZ: '-45deg', top: '5px', left: '0px'} //X形
+      close: {width: '50%', rotateZ: '-45deg', top: '2px', left: '0px'} //箭头形
     }
   });
   var sidebarToggleLine2nd = new SidebarToggleLine({
     el: '.sidebar-toggle-line-middle',
     status: {
       arrow: {width: '90%'},
-      close: {opacity: 0}
+      // close: {opacity: 0} //X形
+      close: {width: '90%'} //箭头形
     }
   });
   var sidebarToggleLine3rd = new SidebarToggleLine({
     el: '.sidebar-toggle-line-last',
     status: {
-      arrow: {width: '50%', rotateZ: '45deg', top: '-2px'},
-      close: {width: '100%', rotateZ: '45deg', top: '-5px'}
+      arrow: {width: '50%', rotateZ: '-45deg', top: '-2px', left: '6px'},
+      // close: {width: '100%', rotateZ: '45deg', top: '-5px', left: '0px'} //X形
+      close: {width: '50%', rotateZ: '45deg', top: '-2px', left: '0px'} //箭头形
     }
   });
+
 
   sidebarToggleLines.push(sidebarToggleLine1st);
   sidebarToggleLines.push(sidebarToggleLine2nd);
